a3e0a0ff139f3dd96914ec815fb80ddabffbbf29
Mateus Pierre <mateus.mateus30@gmail.com>
2025-02-14 10:55:35 -0300

    Update Credentials.ts

--- src/credentials/Credentials.ts
+++ src/credentials/Credentials.ts
@@ -1,7 +1,7 @@
 const Credentials = () => {
   return {
-    ClientId: '86fdd9099a3547b988b31c9c8b284154',
-    ClientSecret: '2985d1ee9f0b4c2fadbab0bcd2aca5f5',
+    ClientId: 'CLIENT_ID',
+    ClientSecret: 'CLIENT_SECRET',
   };
 };
 
